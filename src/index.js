f046b08836294884cf35a32a2cdba12cd5e01817
xWafl <73sampleperson@gmail.com>
2019-03-20 17:34:58 -0400

    basic buying feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 function O(i) {
     return document.getElementById(i);
 }
+
+function buySoap () {
+    if (player.inventory.money >= player.costs.soap){
+        player.inventory.money -= player.costs.soap;
+        player.inventory.soap++;
+        O("soapAmount").innerHTML = player.inventory.soap;
+        O("moneyAmount").innerHTML = player.inventory.money;
+    }
+}
--- src/stats.js
+++ src/stats.js
@@ -2,5 +2,8 @@ var player = {
     "inventory": {
         "soap": 0,
         "money": 10
+    },
+    "costs": {
+        "soap": 1
     }
 }
