e4651a9fc53fea418375afee2bb1b70ddd4ac090
Maclab 2 <maclab2@B-LAB-10600-02.local>
2019-03-26 14:25:54 -0400

    imports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 
+function O(i) {
+    return document.getElementById(i);
+}
+
+function buySoap () {
+    if (player.inventory.money >= player.costs.soap){
+        player.inventory.money -= player.costs.soap;
+        player.inventory.soap++;
+        O("soapAmount").innerHTML = player.inventory.soap;
+        O("moneyAmount").innerHTML = player.inventory.money;
+    }
+}
---
+++ src/stats.js
@@ -0,0 +1,9 @@
+var player = {
+    "inventory": {
+        "soap": 0,
+        "money": 10
+    },
+    "costs": {
+        "soap": 1
+    }
+}
