852ae953a1637c9ec3b8e71cafe65f468f86084c
Maclab 2 <maclab2@B-LAB-10600-02.local>
2019-03-26 14:49:13 -0400

    sell soap modal starting to work

---
+++ src/modals.js
@@ -0,0 +1,24 @@
+var sellSoapMenuModal = document.getElementById('sellSoapMenuModal');
+
+// Get the button that opens the modal
+var sellSoapButton = document.getElementById("sellSoapButton");
+
+// Get the <span> element that closes the modal
+var sellSoapCloseSpan = document.getElementsByClassName("sellSoapMenuModalClose")[0];
+
+// When the user clicks on the button, open the modal
+sellSoapButton.onclick = function() {
+  sellSoapMenuModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+sellSoapCloseSpan.onclick = function() {
+  sellSoapMenuModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == sellSoapMenuModal) {
+    sellSoapMenuModal.style.display = "none";
+  }
+}
